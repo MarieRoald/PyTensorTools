4296d8f03a8e2d50158ba7797f250a4a57d69cc6
Marie Roald <roald.marie@gmail.com>
2019-04-22 15:02:44 +0200

    Added visualisers

--- run_evaluator.py
+++ run_evaluator.py
@@ -1,3 +1,4 @@
+#!/home/marie/anaconda3/bin/python
 import sys
 sys.path.append('/home/marie/Dropbox/Programming/Simula/PyTensor_classification/')
 import pytensor.base
@@ -11,16 +12,52 @@ if __name__ == '__main__':
         {'type': 'ExplainedVariance', 'arguments': {}},
         {'type': 'PValue', 
          'arguments': {
-             'mode': 0,
-             'decomposer_type': 'CP_ALS',
+             'mode': 0
          }
         },
-        {'type': 'WorstDegeneracy', 
-         'arguments': {
-             'decomposer_type': 'CP_ALS',
-         }
+        {'type': 'WorstDegeneracy',
+          'arguments': {}
+        },
+        {'type': 'CoreConsistency',
+          'arguments': {}
+        },
+        #{'type': 'MaxKMeansAcc', 
+        # 'arguments': {
+        #     'matlab_scripts_path': 'pytensortools/evaluation/legacy_matlab_code',
+        #     'mode': 0
+
+        # }
+        #}
+    ]
+    multi_run_evaluators = [
+      {'type': 'Uniqueness', 'arguments': {}}
+    ]
+    single_run_visualisers = [
+        {
+            'type': 'FactorLinePlotter', 
+            'arguments': {
+                'modes': [0, 1, 2]
+            }
+        },
+        {
+            'type': 'FactorScatterPlotter', 
+            'arguments': {
+                'mode': 0
+            }
+        },
+        {
+            'type': 'LogPlotter',
+            'arguments': {
+                'log_name': 'ExplainedVariance',
+                'logger_name': 'ExplainedVarianceLogger',
+                'filename': 'explained_variance'
+            }
         }
     ]
 
-    evaluator = ExperimentEvaluator(single_run_evaluators)
+    evaluator = ExperimentEvaluator(
+        single_run_evaluator_params=single_run_evaluators,
+        multi_run_evaluator_params=multi_run_evaluators,
+        single_run_visualiser_params=single_run_visualisers,
+    )
     evaluator.evaluate_experiment('test_result_for_evaluation_test')
