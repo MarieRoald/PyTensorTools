34dfc7017dd326d33ec98198bed552b29f1c7d53
Marie Roald <roald.marie@gmail.com>
2019-05-15 16:04:10 +0200

    Ensuring that tensor and classes are arrays

--- pytensortools/datareader.py
+++ pytensortools/datareader.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractproperty, abstractmethod
-from scipy.io import loadmat 
+from scipy.io import loadmat
+import numpy as np
 
 # en liste med classes for hver mode?
 # En liste med dictionaries med labelinfo som f.eks. site for hver mode?
@@ -31,10 +32,10 @@ class MatlabDataReader(BaseDataReader):
         """
         super().__init__(mode_names=mode_names)
         self.file_path = file_path
-        self._tensor = loadmat(file_path, variable_names=[tensor_name])[tensor_name]
+        self._tensor = np.array(loadmat(file_path, variable_names=[tensor_name])[tensor_name])
         
         if classes is not None:
             self._classes = [{} for _ in self._tensor.shape]
             for class_dict, mode_classes in zip(self._classes, classes):
                 for name, varname in mode_classes.items():
-                    class_dict[name] = loadmat(file_path, variable_names=[varname])[varname]
+                    class_dict[name] = np.array(loadmat(file_path, variable_names=[varname])[varname])
