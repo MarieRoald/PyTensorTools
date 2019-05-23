5373d400a1a6d38402986dabc82956defa2fb06d
Marie Roald <roald.marie@gmail.com>
2019-05-23 14:45:39 +0200

    Add HDF5DataReader

--- pytensortools/datareader.py
+++ pytensortools/datareader.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractproperty, abstractmethod
 from scipy.io import loadmat
 import numpy as np
+from scipy.stats import ttest_ind
+import h5py
 
 # en liste med classes for hver mode?
 # En liste med dictionaries med labelinfo som f.eks. site for hver mode?
@@ -39,3 +41,34 @@ class MatlabDataReader(BaseDataReader):
             for class_dict, mode_classes in zip(self._classes, classes):
                 for name, varname in mode_classes.items():
                     class_dict[name] = np.array(loadmat(file_path, variable_names=[varname])[varname])
+
+
+class HDF5DataReader(BaseDataReader):
+    def _load_data_tensor(self, file_path, tensor_name):
+        with h5py.File(file_path) as h5:
+            return h5[tensor_name][...]
+
+    def _load_class(self, file_path, class_name):
+        with h5py.File(file_path) as h5:
+            return h5[class_name][...]
+
+    def _load_meta_data(self, file_path, classes):
+        _classes = [{} for _ in self._tensor.shape]
+        for class_dict, mode_classes in zip(self._classes, classes):
+            for name, varname in mode_classes.items():
+                class_dict[name] = self._load_class(file_path, varname)
+        return _classes    
+        
+    def __init__(self, file_path, tensor_name, meta_info_path=None, classes=None, mode_names=None):
+        super().__init__(mode_names=mode_names)
+        self.file_path = file_path
+
+        if meta_info_path is None:
+            meta_info_path = file_path
+
+        self.meta_info_path = meta_info_path
+        self._tensor = self._load_data_tensor(self.file_path, tensor_name)
+
+        if classes is not None:
+            self._classes = self._load_meta_data(self.meta_info_path, classes)
+
