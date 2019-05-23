67d609917cb6120e0cf61756c415e7cb37d588a8
Marie Roald <roald.marie@gmail.com>
2019-05-23 17:08:03 +0200

    Added string support in hdf5 metadata

--- pytensortools/datareader.py
+++ pytensortools/datareader.py
@@ -50,7 +50,10 @@ class HDF5DataReader(BaseDataReader):
 
     def _load_class(self, file_path, class_name):
         with h5py.File(file_path) as h5:
-            return h5[class_name][...]
+            class_data = h5[class_name]
+            if 'is_string' in class_data.attrs and class_data.attrs['is_string']:
+                return [class_data.attrs['string_values'][i] for i in class_data]
+            return class_data[...]
 
     def _load_meta_data(self, file_path, classes):
         _classes = [{} for _ in self._tensor.shape]
