ffd10019a6e7ac596e532fda6d35363872454b6e
Marie Roald <roald.marie@gmail.com>
2019-07-18 18:28:42 +0200

    Added support for hdf5 exporting of factors

--- pytensortools/io/factor_export.py
+++ pytensortools/io/factor_export.py
@@ -13,7 +13,19 @@ def export_components(experiment_path, run, out_name='factors.mat'):
     with open_run(experiment_path, run) as h5:
         checkpoint_group = load_best_group(h5)
         factors = {name: dataset[...] for name, dataset in checkpoint_group.items()}
-    savemat(experiment_path/out_name, factors)
+        attrs = {name: val for name, val in checkpoint_group.attrs.items()}
+    
+    filetype = out_name.split('.')[-1].lower()
+    if filetype == 'mat':
+        savemat(experiment_path/out_name, factors)
+    elif filetype == 'h5' or filetype == 'hdf5':
+        with h5py.File(experiment_path/out_name, 'w') as h5:
+            for name, value in factors.items():
+                h5[name] = value
+                for name, val in attrs.items():
+                    h5.attrs[name] = val
+    else:
+        raise ValueError(f'Invalid file type "{filetype}"')
 
 
 def export_best_components(experiment_path, out_name='factors.mat'):
