0fe54623910905671c81bcdaf50fc7641ad111b9
Marie Roald <roald.marie@gmail.com>
2019-08-11 18:56:29 +0200

    Added setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name="pytensortools",
+    version="0.0.1",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+)
+
