5fef33471b63a2d0c8f8e8c001121ae46e2a273d
Marie Roald <roald.marie@gmail.com>
2019-08-17 19:21:19 +0200

    updated version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name="TensorKit-tools",
-    version="0.0.1",
+    version="0.0.1a",
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
