8400f836541f3f900975cb2cfe1ad681d5f77ea0
Marie Roald <roald.marie@gmail.com>
2020-03-14 17:03:51 +0100

    Updated to fix bugs

--- scripts/crna_experiment.py
+++ scripts/crna_experiment.py
@@ -8,27 +8,33 @@ from scipy.io import loadmat
 
 import tenkit_tools.experiment
 
+NUM_THREADS = 64
 
 DATA_PATH = Path("/home/mariero/datasets/CRNA/missing_060/datasets")
 SAVE_PATH = Path("/work/mariero/experiment_logs/CRNA/first_smoothness/")
 
 RANK = 2
 MAX_ITS = 5000
+NUM_RUNS = 20
+START_DATASET = 0
+END_DATASET = 100
 
 RIDGE = 0.01
+REGS = [0] + np.logspace(1, 7, 12).tolist()
+SIMILARITY_IDS = ["freq_sigma_0_08_h5",  "freq_sigma_0_10_h5",  "freq_sigma_0_20_h5",]
 
 
 def get_save_path(dataset_id, similarity_id):
     similarity_name = Path(similarity_id).stem
 
-    return SAVE_PATH/similarity_name
+    return str(SAVE_PATH/similarity_name)
 
 
-def get_experiment_params(dataset_id, similarity_id):
+def get_experiment_params(dataset_id, similarity_id, reg):
     params = dict(
-        experiment_name = Path(dataset_id).stem,
+        experiment_name = f"{Path(dataset_id).stem}_reg_{reg:.02g}",
         save_path = get_save_path(dataset_id, similarity_id),
-        num_runs = 20,
+        num_runs = NUM_RUNS,
         num_processess = 1,
     )
 
@@ -47,7 +53,7 @@ def get_data_reader_params():
     data_reader_params = {
         "type": "MatlabDataReader",
         "arguments": {
-            "file_path": DATA_PATH/"data_clean.mat",
+            "file_path": str(DATA_PATH/"data_clean.mat"),
             "tensor_name": "data",
             "mode_names": ["node", "hour", "week", "month"],
         }
@@ -78,8 +84,8 @@ def get_decomposition_params(dataset_id, similarity_id, reg, ridge):
             "max_its": MAX_ITS,
             "checkpoint_frequency": 500,
             "convergence_tol": 1e-8,
-            "lower_bounds": None,
-            "upper_bounds": None,
+            "lower_bounds": -np.inf,
+            "upper_bounds": np.inf,
             "factor_constraints": [
                 {'tikhonov_matrix': reg*similarity, 'ridge': ridge},
                 {'ridge': ridge},
@@ -99,20 +105,22 @@ def run_experiment(experiment):
 
 if __name__ == "__main__":
     experiments = []
-    for similarity_id in ["freq_sigma_0_08_h5",  "freq_sigma_0_10_h5",  "freq_sigma_0_20_h5",]:
-        for dataset_num in range(20):
-            for reg in [100, 1000, 10_000]:
-                dataset_id = f"missing_060_{dataset_num+1:03d}.mat"
-
+    for similarity_id in SIMILARITY_IDS:
+        for dataset_num in range(START_DATASET, END_DATASET):
+            dataset_id = f"missing_060_{dataset_num+1:03d}.mat"
+            Path(get_save_path(dataset_id, similarity_id)).mkdir(exist_ok=True, parents=True)
+            for reg in REGS:
                 experiments.append({
-                    "experiment_params": get_experiment_params(dataset_id, similarity_id),
+                    "experiment_params": get_experiment_params(dataset_id, similarity_id, reg),
                     "log_params": get_log_params(),
                     "data_reader_params": get_data_reader_params(),
                     "decomposition_params": get_decomposition_params(dataset_id, similarity_id, reg, RIDGE),
                     "preprocessor_params": []
                 })
     
-    with multiprocessing.Pool(64) as p:
+    # import tqdm 
+    # tasks = [run_experiment(experiment) for experiment in tqdm.tqdm(experiments)]
+    with multiprocessing.Pool(NUM_THREADS) as p:
         tasks = [p.apply_async(run_experiment, [experiment]) for experiment in experiments]
 
         prev_ready = 0
@@ -126,7 +134,3 @@ if __name__ == "__main__":
         results = [task.get() for task in tasks]
     print(results)
     
-
-    
-
-            
